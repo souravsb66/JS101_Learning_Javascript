3a34003c31a613d1c56f3b951be43d83eeef6a40
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-10 22:44:23 +0530

    Innitial commit for problem 4.

---
+++ Introduction_to_JS (Variables)/problem4.js
@@ -0,0 +1,28 @@
+const name = "Sourav Biswas";
+const school = "Sacred Heart High School";
+const grade = 5;
+const section = 'B';
+const rollNo = 31;
+const marksEnglish = 90;
+const marksScience = 84;
+const marksComputer = 98;
+
+//The Report Card
+console.log("***************************************************");
+console.log("\t\t\t▛▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▔▜");
+console.log("\t\t\t▏ "+school+" ▕");
+console.log("\t\t\t▙▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▟");
+
+console.log("\n\t\t\t\t -: REPORT CARD :-\n")
+console.log("---------------------------------------------------");
+console.log("Name:\t    "+name);
+console.log("Grade:\t    "+grade);
+console.log("Section:\t"+section);
+console.log("Roll No:\t"+rollNo);
+console.log("---------------------------------------------------");
+console.log("\nSubject\t   Marks");
+console.log("-------\t   -----\n");
+console.log("English\t    "+marksEnglish);
+console.log("Science\t    "+marksScience);
+console.log("Computer\t"+marksComputer);
+console.log("___________________________________________________");
