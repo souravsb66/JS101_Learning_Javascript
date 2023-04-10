424b8aca7f6b912f20a16675d6783f6adefb97b4
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-11 04:47:52 +0530

    Innitialized two variables start and end. Created a for loop which runs from start to end incrementing with one. Printing start everytime, thus printing all numbers from start to end.

---
+++ While_Loops/problem1.js
@@ -0,0 +1,7 @@
+// Problem 1: Print the numbers from the given starting point till ending point (including both start and end)
+
+let start = 3 , end = 19;
+
+for(;start <= end; start++) {
+  console.log(start);
+}
