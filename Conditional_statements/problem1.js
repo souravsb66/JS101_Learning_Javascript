3c6d54c775fa21d50f353e73c7b97221905a1d85
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-10 23:01:14 +0530

    Innitial commit for problem 1.

---
+++ Conditional_statements/problem1.js
@@ -0,0 +1,9 @@
+//Problem 1: If the number is divisible by 3, print a "multiple of 3".
+
+let number = 15;
+
+if(number % 3 == 0) {
+  console.log("multiple of 3");
+} else {
+  console.log("not a multiple of 3");
+}
