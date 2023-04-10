712023971cb3a2eaf43c6c41654b51e6e2386a44
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-11 04:52:26 +0530

    Innitialized a variable limit. Running a loop from 1 to the limit. Checking if the numbers leave a remainder of 1 when divided by 2. If, yes logging the numbers.

---
+++ While_Loops/problem2.js
@@ -0,0 +1,9 @@
+// Problem 2: Print the odd numbers from 0 till the given limit
+
+let limit = 50;
+
+for(let index = 1; index <= 50; index++) {
+  if(index % 2 != 0) {
+    console.log(index);
+  }
+}
