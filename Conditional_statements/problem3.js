45cde931af0fe37fe67aa6664effd22af6eab671
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-10 23:08:22 +0530

    Innitial commit for problem3.

---
+++ Conditional_statements/problem3.js
@@ -0,0 +1,11 @@
+//Problem 3: Given 2 numbers a and b print which is greater or "both equal".
+
+let a = 77, b = 77;
+
+if(a>b) {
+  console.log("A is greater.");
+} else if(b>a) {
+  console.log("B is greater.");
+} else {
+  console.log("Both are equal.");
+}
