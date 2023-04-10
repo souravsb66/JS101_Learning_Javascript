b90dfaab87e2a7cd3d81f50040e20c07218c621b
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-10 22:31:41 +0530

    Innitial commit for problem1

---
+++ Introduction_to_JS (Variables)/problem1.js
@@ -0,0 +1,7 @@
+//Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+let a = "Masai School";
+let b = "A Transformation in Education";
+
+console.log(a);
+console.log(b);
