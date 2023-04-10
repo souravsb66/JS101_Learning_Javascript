e44d6aa6adcb04c7087c1abcb722ffcb8f65a645
Sourav Biswas <sourav.sb56@gmail.com>
2023-04-10 22:58:52 +0530

    Innitial commit for traffic.

---
+++ Conditional_statements/traffic.js
@@ -0,0 +1,11 @@
+let signal = "white"; //Current Signal light
+
+if(signal == "red") {
+  console.log("Stop");
+} else if(signal == "green") {
+  console.log("Go");
+} else if(signal == "yellow") {
+  console.log("Slow Down");
+} else {
+  console.log("Error");
+}
